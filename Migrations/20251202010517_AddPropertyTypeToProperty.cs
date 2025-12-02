12c5f44d568d71967f088c4fb91572f31a7b465a
Bryan Espejo <U202213278@upc.edu.pe>
2025-12-01 20:15:19 -0500

    Add PropertyType to Property

---
+++ Migrations/20251202010517_AddPropertyTypeToProperty.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiVivienda.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPropertyTypeToProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PropertyType",
+                table: "Properties",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PropertyType",
+                table: "Properties");
+        }
+    }
+}
